cb4050fb68514cf0d1454c337d52f158edcef707
Chinecherem <chinecheremubawike@gmail.com>
2024-03-28 16:13:14 +0100

    New changes

--- src/routes/admin_dashboard/dashboard/Dashboard.jsx
+++ src/routes/admin_dashboard/dashboard/Dashboard.jsx
@@ -4,6 +4,11 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 import styles from './Dashboard.module.css';
 
+import users from './asset/users.svg'
+import transaction from './asset/transaction.svg';
+import transaction_2 from './asset/transaction_2.svg';
+
+
 import AdminSidebar from '../../../components/admin/adminSidebar/AdminSidebar';
 import DashboardNavbar from '../../../components/general/dashboard_navbar/DashboardNavbar';
 import axios from 'axios';
@@ -16,32 +21,58 @@ export default function AdminDashboard() {
     <Container fluid className={`${styles.admin_dashboard}`}>
         <Row>
             <AdminSidebar active="Dashboard"/>
-            <Col className='offset-sm-2 offset-lg-3'>
+            <Col className='offset-sm-2 offset-lg-3 ps-3 pe-4'>
                <DashboardNavbar />
                 <Row className='mt-4'>
                     <Col><p className={`${styles.welcome}`}>Welcome Claire,</p></Col>
                 </Row>
+
+                <div className='d-flex flex-wrap justify-content-between'>
+                  <div className={`${styles.admin_dashboard_widget}`}>
+                      <div className='d-flex flex-column align-items-center'>
+                          <div className={`${styles.widget_icon}`}><img src={users} className='d-block w-100' alt="" /></div>
+                          <div className={`${styles.widget_title}`}>Total number of registered Users</div>
+                          <div className={`${styles.widget_value}`}>29</div>
+                          
+                      </div>
+                  </div>
+                  <div className={`${styles.admin_dashboard_widget}`}>
+                      <div className='d-flex flex-column align-items-center'>
+                          <div className={`${styles.widget_icon}`}><img src={transaction} className='d-block w-100' alt="" /></div>
+                          <div className={`${styles.widget_title}`}>Total number of transactions</div>
+                          <div className={`${styles.widget_value}`}>29</div>
+                          
+                      </div>
+                  </div>
+                  <div className={`${styles.admin_dashboard_widget}`}>
+                      <div className='d-flex flex-column align-items-center'>
+                          <div className={`${styles.widget_icon}`}><img src={transaction_2} className='d-block w-100' alt="" /></div>
+                          <div className={`${styles.widget_title}`}>Total number of successful transaction</div>
+                          <div className={`${styles.widget_value}`}>29</div>
+                          
+                      </div>
+                  </div>
+                  <div className={`${styles.admin_dashboard_widget}`}>
+                      <div className='d-flex flex-column align-items-center'>
+                          <div className={`${styles.widget_icon}`}><img src={transaction_2} className='d-block w-100' alt="" /></div>
+                          <div className={`${styles.widget_title}`}>Total number of pending transaction</div>
+                          <div className={`${styles.widget_value}`}>29</div>
+                          
+                      </div>
+                  </div>
+                  <div className={`${styles.admin_dashboard_widget}`}>
+                      <div className='d-flex flex-column align-items-center'>
+                          <div className={`${styles.widget_icon}`}><img src={transaction_2} className='d-block w-100' alt="" /></div>
+                          <div className={`${styles.widget_title}`}>Total amount generated</div>
+                          <div className={`${styles.widget_value}`}>29</div>
+                          
+                      </div>
+                  </div>
+                </div>
+
+
+             
                 <Row className='d-none'>
-                  <Col xs = "4" className=''>
-                    <div className={`${styles.dashboard_card}`}>
-                        
-                    </div>
-                  
-                  </Col>
-                  <Col xs = "4" className=''>
-                    <div className={`${styles.dashboard_card}`}>
-                        
-                    </div>
-                  
-                  </Col>
-                  <Col xs = "4" className=''>
-                    <div className={`${styles.dashboard_card}`}>
-                        
-                    </div>
-                  
-                  </Col>
-                </Row>
-                <Row>
                   <Col md = '6' lg = '4' className='mb-lg-4' >
                     <div className={`${styles.todays_hunt}`}>
                         <h3 className='mb-4'>Today's hunt</h3>
